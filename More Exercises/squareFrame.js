86984d7e540450674677ac01ba04e4ac21b62a85
Myrkoni <pi4aaa@abv.bg>
2023-02-14 21:01:28 +0200

    lesson

---
+++ More Exercises/squareFrame.js
@@ -0,0 +1,21 @@
+function solve(input) {
+    let n = Number(input[0]);
+    let result = "";
+    let result2 = "";
+
+    for (let i = 1; i <= n; i++) {
+        for (let e = 1; e <= n; e++) {
+            if((i === 1 && e === 1) || (i === n && e ===n) || (i === 1 && e === n) || (i === n && e === 1)){
+                result = result + `${"+"} `;
+            }else if((i > 1 && e === n) || (i > 1 && e === 1)){
+                result = result + `${"|"} `;
+            }else{
+                result = result + `${"-"} `;
+            }
+        }
+        
+        console.log(result);
+        result = "";
+    }
+}
+solve([4])
