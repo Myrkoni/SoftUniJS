bfa6f4df901c66edef8f4088ca79b05e787b8ebb
Myrkoni <pi4aaa@abv.bg>
2023-03-22 20:03:46 +0200

    lesson

--- Basic/demo.js
+++ Basic/demo.js
@@ -1,24 +1,20 @@
-function solve(number){
-    let divisibleNumber = 0;
-    if(number % 2 === 0){
-        divisibleNumber = 2;
+function solve(arrey){
+    let userName = arrey[0];
+    let password = "";
+    for(let i = userName.length - 1; i >= 0; i--){
+        password +=`${userName.charAt(i)}`;
     }
-    if(number % 3 === 0){
-        divisibleNumber = 3;
-    }
-    if(number % 6 === 0){
-        divisibleNumber = 6;
-    }
-    if(number % 7 === 0 ){
-        divisibleNumber = 7;
-    }
-    if(number % 10 === 0){
-        divisibleNumber = 10;
-    }
-    if(divisibleNumber !== 0){
-    console.log(`The number is divisible by ${divisibleNumber}`);
-    }else{
-        console.log("Not divisible");
+    for(let i = 1; i < arrey.length; i++){
+        let correctPassword = arrey[i];
+        if(correctPassword === password){
+            console.log(`User ${userName} logged in.`);
+        }else if(correctPassword !== password && i < 4){
+            console.log("Incorrect password. Try again.");
+        }
+        if(i === 4 && correctPassword !== password){
+            console.log(`User ${userName} blocked!`);
+            break;
+        }
     }
 }
-solve(12)
+solve(['sunny','rainy','cloudy','sunny','not sunny']);
