de51729936924a91bafc369a1073b23bbee8b517
Myrkoni <pi4aaa@abv.bg>
2023-06-13 21:02:16 +0300

    lesson

---
+++ Fundamentals/arrays/dungeonestDark.js
@@ -0,0 +1,44 @@
+function dungeonestDark(input) {
+    let eachRoom = input[0].split("|");
+    let health = 100;
+    let coins = 0;
+    let count = 0;
+    let isLife = true;
+
+    for (let el of eachRoom){
+        count++;
+        let command = el.split(" ");
+        let action = command[0];
+        let point = Number(command[1]);
+
+        if(action === "potion"){
+            let healed = (health + point) < 100 ? point : 100 - health;
+            health += point
+            if(health > 100){
+                health = 100;
+            }
+            console.log(`You healed for ${healed} hp.`);
+            console.log(`Current health: ${health} hp.`);
+        }else if(action === "chest"){
+            coins += point;
+            console.log(`You found ${point} coins.`);
+        }else{
+            health -= point;
+            if(health > 0){
+                console.log(`You slayed ${action}.`);
+            }else{
+                console.log(`You died! Killed by ${action}.`);
+                console.log(`Best room: ${count}`);
+                isLife = false;
+                break;
+            }
+        }
+        
+    }
+    if(isLife){
+        console.log("You've made it!");
+        console.log(`Coins: ${coins}`);
+        console.log(`Health: ${health}`);
+    }
+}
+dungeonestDark(["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
